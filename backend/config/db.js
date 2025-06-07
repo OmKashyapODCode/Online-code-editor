f156e75c1dc41ac2df7187d4a648c3b5613b1253
OmKashyapODCode <kashyap.om8585@gmail.com>
2025-06-07 12:15:54 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
 
-    await mongoose.connect("mongodb://127.0.0.1:27017/codeIDE",{
+    await mongoose.connect("mongodb+srv://kashyapom9968:rCvYcgBr35o8V5qw@cluster0.sowv5fo.mongodb.net/CODEIDE",{
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
